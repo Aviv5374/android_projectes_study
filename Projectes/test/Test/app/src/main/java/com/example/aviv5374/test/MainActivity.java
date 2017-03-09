085f8e6007aa85c5f0919eab4ac38b4180c1d2e1
Aviv5374 <Aviv5374@gmail.com>
2017-03-09 11:06:09 +0200

    testing 2

---
+++ Projectes/test/Test/app/src/main/java/com/example/aviv5374/test/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.aviv5374.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testingPush(){
+        int i = 1+1;
+    }
+
+}
