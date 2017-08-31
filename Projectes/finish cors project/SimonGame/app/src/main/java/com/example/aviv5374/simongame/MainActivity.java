e2f85a6e05a4cd6825419599ff3fbf0856060580
Aviv5374 <Aviv5374@gmail.com>
2017-08-31 10:51:08 +0300

    set up simon project 2

--- Projectes/finish cors project/SimonGame/app/src/main/java/com/example/aviv5374/simongame/MainActivity.java
+++ Projectes/finish cors project/SimonGame/app/src/main/java/com/example/aviv5374/simongame/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.aviv5374.simongame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /*
+        switch(item.getItemId()){
+       case R.id.newtGame:
+       startGame();
+      return true;
+       default:
+         */
+        return super.onOptionsItemSelected(item);
     }
 }
