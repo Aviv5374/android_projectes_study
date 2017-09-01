1cb5af57b1ea9bebd3beec26a9c9ef7aa399099f
Aviv5374 <Aviv5374@gmail.com>
2017-09-01 13:38:56 +0300

    set up simon project 9

--- Projectes/finish cors project/SimonGame/app/src/main/java/com/example/aviv5374/simongame/MainActivity.java
+++ Projectes/finish cors project/SimonGame/app/src/main/java/com/example/aviv5374/simongame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.aviv5374.simongame;
 
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -23,33 +24,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttons[0] = (Button)findViewById(R.id.redButton);
-        buttons[1] = (Button)findViewById(R.id.blueButton);
-        buttons[2] = (Button)findViewById(R.id.greenButton);
-        buttons[3] = (Button)findViewById(R.id.yellowButton);
-        buttons[0].setBackgroundResource(R.drawable.lightred);//this ok!
+        buttons[0] = (Button) findViewById(R.id.redButton);
+        buttons[1] = (Button) findViewById(R.id.blueButton);
+        buttons[2] = (Button) findViewById(R.id.greenButton);
+        buttons[3] = (Button) findViewById(R.id.yellowButton);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu,menu);
+        inflater.inflate(R.menu.main_menu, menu);
         return true;
 
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.start_anew_game_op:
-                startGame();
+                //startGame();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    private void startGame(){
+    private void startGame() {
         /*
        int counter = 0;
 if(!isGameRuning){
@@ -75,7 +75,7 @@ toast.show():
          */
     }
 
-    private void manageButtonsSequence(){
+    private void manageButtonsSequence() {
         /*
         chagnButtonsClickableState(in boolean state =false): void//TO CHECK: maybe not relevent
 addToSequencen(): void
@@ -84,16 +84,12 @@ chagnButtonsClickableState(in boolean state =true): void
          */
     }
 
-    private void chagnButtonsClickableState( boolean state){
-        /*
-        for(buttons.Count)
-{
-buttons[i].setClickable(state);
-)
-         */
+    private void chagnButtonsClickableState(boolean state) {
+        for (int i = 0; i < buttons.Count; i++)
+            buttons[i].setClickable(state);
     }
 
-    private void addToSequencen(){
+    private void addToSequencen() {
         /*
         Random rand = new Random();
 int chosenIndex = rand.nextInt(buttons.legth);
@@ -106,63 +102,76 @@ buttonsSequence.Add(buttons[chosenIndex);
          */
     }
 
-private void playSequence(){
+    private void playSequence() {
     /*
     for(buttonsSequence.Count){
 playButton(Button b=buttonsSequence[i]): void
 }
      */
-}
+    }
 
-public void onButtonClick(View v){
-    /*
-    Button relventButton = (Button) v...;
-chagnButtonsClickableState(in boolean state =false): void//?
-playButton(relventButton): void
+    public void onButtonClick(View v) {
+
+        Button relventButton = (Button) v...;
+//chagnButtonsClickableState(in boolean state =false): void//?
+        playButton(relventButton);
+/*
 if(isGameRuning && comperButtons(relventButton): boolean){
 prepareTheNextTry(): void
 }
 else{
 endGame(): void
 }
-chagnButtonsClickableState(in boolean state =true): void//?
-     */
-}
+*/
+//chagnButtonsClickableState(in boolean state =true): void//?
 
-private void playButton(Button button){
-    /*
-    https://www.youtube.com/watch?v=whFVhvM-J0U
-code:
-MediaPlayer mp;
-int idOfReleventButtonOriginalColor;
-int idOfReleventButtonLighterColor;
-swich b{
-case red:
-idOfReleventButtonOriginalColor = red;
-idOfReleventButtonLighterColor = lighterRed
-mp = MediaPlayer.create(this, R.raw.redSong);
-braek;
-case blue ...
+    }
 
-mp.start();
-b.setColor(idOfReleventButtonLighterColor);
-//busy wating
-whlie(mp != null && mp.getCurrentPosition() <  mp.getDuration()){};
-mp.stop();
-//alwys  after stop
-mp = MP.create(...);
-b.setColor(idOfReleventButtonOriginalColor);
-     */
-}
+    private void playButton(Button button) {
+        //https://www.youtube.com/watch?v=whFVhvM-J0U
+        MediaPlayer mp = null;
+        int idOfRelevantButtonOriginalColor = 0;
+        int idOfRelevantButtonLighterColor = 0;
+        switch (button.getId()) {
+            case R.id.redButton:
+                idOfRelevantButtonOriginalColor = R.drawable.red3;
+                idOfRelevantButtonLighterColor = R.drawable.lightred;
+                mp = MediaPlayer.create(this, R.raw.redbutton);
+                break;
+            case R.id.blueButton:
+                idOfRelevantButtonOriginalColor = R.drawable.blue;
+                idOfRelevantButtonLighterColor = R.drawable.lightblue;
+                mp = MediaPlayer.create(this, R.raw.bluebutton);
+                break;
+            case R.id.greenButton:
+                idOfRelevantButtonOriginalColor = R.drawable.green;
+                idOfRelevantButtonLighterColor = R.drawable.lightgreen;
+                mp = MediaPlayer.create(this, R.raw.greenbutton);
+                break;
+            case R.id.yellowButton:
+                idOfRelevantButtonOriginalColor = R.drawable.yellow;
+                idOfRelevantButtonLighterColor = R.drawable.lightyellow;
+                mp = MediaPlayer.create(this, R.raw.yellowbutton);
+                break;
+        }
+        //buttons[0].setBackgroundResource(R.drawable.lightred);//this ok!
+        mp.start();
+        button.setBackgroundResource(idOfRelevantButtonLighterColor);
+//busy waiting
+        whlie(mp != null && mp.getCurrentPosition() < mp.getDuration());
+        mp.stop();
+        button.setBackgroundResource(idOfRelevantButtonOriginalColor);
 
-private boolean comperButtons(Button button){
+    }
+
+    private boolean comperButtons(Button button) {
     /*
     return buttonsSequence[indexOfReleventSequenceButon].equles(b);
      */
-    return true;
-}
+        return true;
+    }
 
-private void prepareTheNextTry(){
+    private void prepareTheNextTry() {
     /*
     indexOfReleventSequenceButon++
 if(indexOfReleventSequenceButon >= buttonsSequence.count ){
@@ -179,16 +188,16 @@ else{
 chagnButtonsClickableState(in boolean state =true): void
 }
      */
-}
+    }
 
-private void endGame(){
+    private void endGame() {
     /*
     Tost tost = Toast.makeText(this,String.valueOf(buttonsSequence.count),Toast.LENGTH_SHORT);
 toast.show();
 isGameRuning = false;
 chagnButtonsClickableState(in boolean state =true): void
      */
-}
+    }
 
 
 }
